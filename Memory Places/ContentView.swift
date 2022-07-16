8987434f7822412fc389db0d4308c0fbb58cdf17
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-07-16 20:36:43 +0700

    Initial Commit

---
+++ Memory Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory Places
+//
+//  Created by Ho Le Minh Thach on 16/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memory Places/Memory_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_PlacesApp.swift
+//  Memory Places
+//
+//  Created by Ho Le Minh Thach on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memory_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
